9125fb5de6c9546a7cdd2a3b46b0b445532346aa
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-07-08 22:23:34 +0200

    finish all parts done

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 
+
+const uri = "https://jsonplaceholder.typicode.com/users";
+
+
 async function obtainUserData() {
-    try {
-        const response = await
-        fetch(' https://jsonplaceholder.typicode.com/users');
+  
+        const response = await fetch(uri);
         const users = await response.json();
+
+        const resultsBody = document.getElementById('resultsBody');
+        users.forEach(user => {
+            const row = `<tr><td>${user.id}</td><td>${user.name}</td><td>${user.address.city}</td></tr>`;
+            resultsBody.insertAdjacentHTML('beforeend', row);
+        });
+
         console.log(users);
 
-    }catch (error) {
-        console.error('Error  obtaining users: '. error);
-    }
 }
+
+obtainUserData();
+
+async function requestUserDataById(id) {
+    const response = await fetch(`${uri}/${id}`);
+    const user = await response.json();
+    document.getElementById('resultData').innerHTML = 'Name: ' + user.name + '<br>Phone: ' + user.phone;
+}
+
+document.querySelector('button').addEventListener('click', () => {
+    const userId = document.getElementById('userId').value;
+    requestUserDataById(userId);
+})
 
 obtainUserData();
